0fb77f41b3b64b4d6610e68e0508db105c33546e
RIC21000 <melericcardo9400@gmail.com>
2022-11-27 20:04:22 +0100

    Begun to add create and join party

--- main.go
+++ main.go
@@ -3,45 +3,23 @@ package main
 import (
 	"errors"
 	"fmt"
-	"image/color"
 	"log"
 	"net"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
 var a fyne.App
-var loginWin fyne.Window
+var mainWindow fyne.Window
 var conn net.Conn
 var pseudoEntry *widget.Entry
 var addressOfServer *widget.Entry
 var portOfServer *widget.Entry
 var isCurrentlyLogin bool
-
-func getButtons() []*fyne.Container {
-	var buttons []*fyne.Container
-	for x := 0; x < 9; x++ {
-		btn1, color1, finalButton1 := getButtonWithColor(color.RGBA{R: 0, G: 0, B: 255})
-		btn1.SetText("test")
-		color1.FillColor = color.RGBA{R: 255}
-		buttons = append(buttons, finalButton1)
-	}
-	return buttons
-}
-
-func undefined() {
-	buttons := getButtons()
-	morpioncontainer := container.NewGridWithRows(3, buttons[0], buttons[1], buttons[2], buttons[3], buttons[4], buttons[5], buttons[6], buttons[7], buttons[8])
-	text := canvas.NewText("Morpion", color.RGBA{R: 255, B: 255})
-	text.Alignment = fyne.TextAlignCenter
-	maincontainer := container.NewBorder(text, nil, nil, nil, morpioncontainer)
-	log.Print(maincontainer)
-}
 
 func getInfos() (address, port, pseudo string, err error) {
 	if pseudoEntry.Text == "" {
@@ -56,21 +34,38 @@ func getInfos() (address, port, pseudo string, err error) {
 	return address, port, pseudo, nil
 }
 
-func SendPseudo() {
+func CreateParty() {
+	log.Println("En développement...")
+}
+
+func JoinParty() {
+	log.Println("En développement...")
+}
+
+func LoginSuccessfully() {
+	mainWindow.Resize(fyne.NewSize(0, 0))
+	createPartyButton := widget.NewButton("Créer une partie", CreateParty)
+	joinPartyButton := widget.NewButton("Rejoindre une partie", JoinParty)
+	mainContainer := container.NewVBox(createPartyButton, joinPartyButton)
+	mainWindow.SetContent(mainContainer)
+}
+
+func SendPseudo() error {
 	conn.Write([]byte(pseudoEntry.Text))
 	isPseudoOk := make([]byte, 1024)
 	n, err := conn.Read(isPseudoOk)
 	if err != nil {
-		dialog.NewError(err, loginWin).Show()
-		return
+		dialog.NewError(err, mainWindow).Show()
+		conn.Close()
+		return errors.New("erreur lors de la lecture de la réponde du serveur")
 	}
 	isPseudoOkString := string(isPseudoOk[:n])
 	if isPseudoOkString != "pseudook" {
-		log.Println(isPseudoOkString)
 		conn.Close()
-		return
+		return errors.New(isPseudoOkString)
 	}
 	fmt.Println("Connecté !")
+	return nil
 }
 
 func login() {
@@ -83,20 +78,25 @@ func login() {
 	var err error
 	address, port, _, err := getInfos()
 	if err != nil {
-		dialog.NewError(err, loginWin).Show()
+		dialog.NewError(err, mainWindow).Show()
 		return
 	}
 	conn, err = net.Dial("tcp", address+":"+port)
 	if err != nil {
-		dialog.NewError(err, loginWin).Show()
+		dialog.NewError(err, mainWindow).Show()
 		return
 	}
-	SendPseudo()
+	err = SendPseudo()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	LoginSuccessfully()
 }
 
 func main() {
 	a = app.NewWithID("Morpion Multijoueur en Go")
-	loginWin = a.NewWindow("Morpion multijoueur")
+	mainWindow = a.NewWindow("Morpion multijoueur")
 	pseudoEntry = widget.NewEntry()
 	pseudoEntry.SetPlaceHolder("Votre pseudo ici")
 	addressOfServer = widget.NewEntry()
@@ -110,24 +110,16 @@ func main() {
 	)
 	form.OnSubmit = login
 	form.OnCancel = a.Quit
-	loginWin.SetCloseIntercept(
+	mainWindow.SetCloseIntercept(
 		dialog.NewConfirm("Quitter ?", "Voulez-vous vraiment quitter le jeu ?", func(b bool) {
 			if b {
-				loginWin.Close()
+				mainWindow.Close()
 			}
-		}, loginWin).Show,
+		}, mainWindow).Show,
 	)
-	loginWin.SetContent(form)
-	loginWin.Resize(fyne.NewSize(400, loginWin.Canvas().Size().Height))
-	loginWin.SetFixedSize(true)
-	loginWin.CenterOnScreen()
-	loginWin.ShowAndRun()
-}
-
-// Inutile pour l'instant
-func getButtonWithColor(c color.Color) (*widget.Button, *canvas.Rectangle, *fyne.Container) {
-	btn := widget.NewButton("", nil)
-	btn_color := canvas.NewRectangle(c)
-	btn_container := container.NewMax(btn_color, btn)
-	return btn, btn_color, btn_container
+	mainWindow.SetContent(form)
+	mainWindow.Resize(fyne.NewSize(400, mainWindow.Canvas().Size().Height))
+	mainWindow.SetFixedSize(true)
+	mainWindow.CenterOnScreen()
+	mainWindow.ShowAndRun()
 }
